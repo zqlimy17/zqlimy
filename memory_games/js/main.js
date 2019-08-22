85660baf899457be06a53c4be8285eafd6e56984
zqlimy17 <zqlimy@gmail.com>
2019-08-22 21:51:15 +0800

    created card variables

--- memory_games/js/main.js
+++ memory_games/js/main.js
@@ -1,7 +1,17 @@
-console.log("Up and running!");
-var cardOne = 'queen'
-var cardTwo = 'king'
-var cardThree = 'king'
-var cardFour = 'queen'
-console.log('User flipped ' + cardOne )
-console.log('User flipped ' + cardThree)
+var cards = ['queen', 'queen', 'king','king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped ' + cardOne)
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardTwo)
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert('You found a match!');
+    } else {
+        alert('Sorry, try again.');
+    }
+}
+console.log(cardsInPlay);
