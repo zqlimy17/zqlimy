d762e407416e3905bef1d0398c9f29fd17815da3
zqlimy17 <zqlimy@gmail.com>
2019-08-23 22:23:56 +0800

    finished game!

--- memory_games/js/main.js
+++ memory_games/js/main.js
@@ -24,21 +24,36 @@ var cardsInPlay = [];
 
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
+        if(
+              setTimeout(function() {confirm("You found a match!");}, 200)) {}
+              else  window.location.reload();
 } else {
-  alert("Sorry, try again.");
+    if (
+          setTimeout(function() {confirm("Sorry, try again.");}, 200)) {}
+          else window.location.reload();
 }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+    var cardId = this.getAttribute('data-id')
     console.log('User flipped ' + cards[cardId].rank)
     cardsInPlay.push(cards[cardId].rank)
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+    this.setAttribute('src', cards[cardId].cardImage)
     if (cardsInPlay.length === 2) {
         checkForMatch();
     };
 };
-flipCard(0);
-flipCard(2);
 
+function createBoard() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard)
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
